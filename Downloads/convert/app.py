defaf978edd0c8d17d58de90d5bf8a3d531afce5
Akilan k <96933500+Akil81485@users.noreply.github.com>
2025-09-21 03:06:57 +0530

    app.py

--- Downloads/convert/app.py
+++ Downloads/convert/app.py
@@ -116,4 +116,6 @@ def files(filename):
     return send_from_directory(DOWNLOAD_DIR, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
